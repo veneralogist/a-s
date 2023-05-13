5ff115ed4d562f5ad4d19c6ceb09c12c27c3bc16
veneralogist <bilimsel314@gmail.com>
2023-05-13 16:57:14 +0300

    dördüncü seviye

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 const app = express();
 
 app.set("view engine", "ejs");
@@ -18,49 +19,55 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-
-
-const User = new mongoose.model("User",userSchema)
+const User = new mongoose.model("User", userSchema);
 
 app.get("/", function (req, res) {
   res.render("home");
 });
 app.get("/login", function (req, res) {
-    res.render("login");
+  res.render("login");
 });
-  app.get("/register", function (req, res) {
-    res.render("register");
+app.get("/register", function (req, res) {
+  res.render("register");
 });
 
-app.post("/register",(req,res)=>{
-const newUser = new User({
-  email: req.body.username,
-  password: md5(req.body.password),
+app.post("/register", (req, res) => {
+  bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash,
+    });
+    newUser
+      .save()
+      .then(() => {
+        res.render("secrets");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
 });
-newUser.save().then(()=>{
-    res.render("secrets")
-}).catch((err)=>{
-    console.log(err)
-})
-})
-app.post("/login",(req,res)=>{
-    const username=req.body.username;
-    const password = md5(req.body.password);
-
-    User.findOne({email:username}).then((foundUser)=>{
-        if(foundUser){
-            if(foundUser.password===password){
-                res.render("secrets")
-            }else{
-                res.redirect("/login")
-            }
-        }else{
-            res.redirect("/login")
-        }
-    }).catch((err)=>{
-        console.log(err)
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  User.findOne({ email: username })
+    .then((foundUser) => {
+      if (foundUser) {
+        bcrypt.compare(password, foundUser.password, (err, result) => {
+          if (result === true) {
+            res.render("secrets");
+          } else {
+            res.redirect("/login");
+          }
+        });
+      } else {
+        res.redirect("/login");
+      }
     })
-})
+    .catch((err) => {
+      console.log(err);
+    });
+});
     
 
 
